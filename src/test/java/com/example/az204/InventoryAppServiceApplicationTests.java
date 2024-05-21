f38d6a91dac42fcf2c4095ba86b52437320c7d15
Pierre Laurens <pierrelaurens@kinandcarta.com>
2024-05-21 15:14:24 -0500

    init commit

---
+++ src/test/java/com/example/az204/InventoryAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.az204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
